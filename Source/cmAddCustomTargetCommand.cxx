c4ae3ea61987fdcefb9264272cd9bdb02b5a58a2
n59321jh <joshua.hines@student.manchester.ac.uk>
2024-08-09 13:57:16 +0100

    initial workflow test

--- Source/cmAddCustomTargetCommand.cxx
+++ Source/cmAddCustomTargetCommand.cxx
@@ -1,3 +1,4 @@
+// Test with command
 /* Distributed under the OSI-approved BSD 3-Clause License.  See accompanying
    file Copyright.txt or https://cmake.org/licensing for details.  */
 #include "cmAddCustomTargetCommand.h"
